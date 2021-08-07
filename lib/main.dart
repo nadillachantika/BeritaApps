e5b0a199f95fcf6f4fa390a5066c38075e7410c9
Nadilla <nadilla.c.putri@gmail.com>
2021-08-07 09:09:38 +0700

    first commmit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 import 'package:berita_app/detail_berita.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 void main() {
   runApp(MyApp());
 }
@@ -16,7 +17,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: BeritaHome(),
@@ -25,13 +25,12 @@ class MyApp extends StatelessWidget {
 }
 
 class BeritaHome extends StatelessWidget {
-
   Future<Map<String, dynamic>> getBerita() async {
     final response = await http.get(Uri.parse(
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=be14be3a5ea949858c0c15edca7cd40d"));
+        "https://newsapi.org/v2/everything?q=tesla&from=2021-07-06&sortBy=publishedAt&apiKey=be14be3a5ea949858c0c15edca7cd40d"));
     return jsonDecode(response.body);
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +43,6 @@ class BeritaHome extends StatelessWidget {
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
             return _showBerita(snapshot.data['articles']);
-
           } else if (snapshot.hasError) {
             return Center(
               child: Text("Error ${snapshot.error.toString()}"),
@@ -59,68 +57,69 @@ class BeritaHome extends StatelessWidget {
     );
   }
 
-  Widget _showBerita(List data){
+  Widget _showBerita(List data) {
     return ListView.builder(
       itemCount: data.length,
-      itemBuilder: (BuildContext context, index)
-
-      {
-
+      itemBuilder: (BuildContext context, index) {
         return Container(
-
-          margin: EdgeInsets.all(12.0),
-          padding: EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12.0),
-            boxShadow: [
-              BoxShadow (
-                color: Colors.black12,
-                blurRadius: 3.0,
-              ),
-            ]
-          ),
-          child: Card(
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => PageDetailBerita(data: data[index])));
-              },
-        child:Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 200.0,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image : NetworkImage('${data[index]['urlToImage']}'),
-                    fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(12.0),
+            margin: EdgeInsets.all(5.0),
+            padding: EdgeInsets.all(2.0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:  BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 3.0,
                   ),
-          ),
-          SizedBox(height: 8.0),
-          Container(
-            padding: EdgeInsets.all(6.0),
-              decoration: BoxDecoration(
-          //    color: Colors.red,
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-            child: Text( '${data[index]['title']}',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            fontSize: 16.0,
-            )
+                ]),
+            child: Card(
+                child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  PageDetailBerita(data: data[index])));
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 200.0,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    '${data[index]['urlToImage']}'),
+                                fit: BoxFit.cover),
+                            borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(5),
+                              topLeft: Radius.circular(5),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Container(
+                          padding: EdgeInsets.all(6.0),
+                          decoration: BoxDecoration(
+                            //    color: Colors.red,
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: Text('${data[index]['title']}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                              ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ),
             ),
-          )
-            ],
-          )
-        )));
+        );
       },
     );
   }
 }
-
